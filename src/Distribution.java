7f93b9f9ddb92026c16250ef5e8ad46368c99dc7
sylviafrank8 <37809163+sylviafrank8@users.noreply.github.com>
2019-03-07 22:31:38 -0500

    Update Distribution.java

--- src/Distribution.java
+++ src/Distribution.java
@@ -4,22 +4,32 @@ import java.util.ArrayList;
 
 public class Distribution {
 
-	public static int uniformDist(){
-		int d = ThreadLocalRandom.current().nextInt(1,1001);
-		return d;
-	}
+	ArrayList<Integer> dist;
 
-	public static int zipfDist(){
-		//creates an ArrayList that has 1000 values of 1, 500 values of 2, ... etc. such that choosing a 
-		//random element of the ArrayList will give you a value with probability according to Dipf distribution
-		ArrayList<Integer> dist = new ArrayList<Integer>();
+	public Distribution(){
+
+		//necessary to create list for zipf
+
+		dist = new ArrayList<Integer>();
 		for (int i=1; i<=1000; i++){
 			int p = (int)(1000.0*(1.0/i)); 
 			for (int j=1; j<=p; j++) {
 				dist.add(i);
 			}
 		}
-		int d = dist.get(ThreadLocalRandom.current().nextInt(0,dist.size()));
+
+	} //constructor
+
+	public static int uniformDist(){
+		int d = ThreadLocalRandom.current().nextInt(1,1001);
 		return d;
+	}
+
+	public int zipfDist(){
+
+		//creates an ArrayList that has 1000 values of 1, 500 values of 2, ... etc. such that choosing a 
+		//random element of the ArrayList will give you a value with probability according to Dipf distribution
+		return dist.get(ThreadLocalRandom.current().nextInt(0,dist.size()));
+		
 	}
 }
